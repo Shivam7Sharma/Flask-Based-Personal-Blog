0b4c4960bbd556da33e81dd5ed15bb1599f8c21f
shivam7sharma <ssharma4@wpi.edu>
2024-11-24 17:44:33 -0500

    Fix circular import issue

--- application/models.py
+++ application/models.py
@@ -1,10 +1,12 @@
 from datetime import datetime
-from app import db, login_manager
+from application import db, login_manager
 from flask_login import UserMixin
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,13 +15,14 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    
     def __repr__(self):
         return f'Post("{self.title}", "{self.date_posted}")'
