ef3d18faec26ea5f6bd8f8b98c5e98ef76fe1e25
shivam7sharma <ssharma4@wpi.edu>
2024-11-24 15:34:05 -0500

    Fixed naming conventions

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,30 @@
 
-# from app import routes
-# from app.routes import *
+# from application import routes
+# from application.routes import *
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_pymongo import PyMongo
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key_here'
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flaskuser:flaskpassword@localhost/blogdb'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['MONGO_URI'] = "mongodb://localhost:27017/blogdb_nosql"
+application = Flask(__name__)
+application.config['SECRET_KEY'] = 'your_secret_key_here'
+application.config['DEBUG'] = True
+application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flaskuser:flaskpassword@localhost/blogdb'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+application.config['MONGO_URI'] = "mongodb://localhost:27017/blogdb_nosql"
 
 
-mongo = PyMongo(app)
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+mongo = PyMongo(application)
+db = SQLAlchemy(application)
+bcrypt = Bcrypt(application)
+login_manager = LoginManager(application)
 login_manager.login_view = 'login'
 
 
 # Inline import to avoid circular imports
 def register_routes():
-    from app import routes
+    from application import routes
 
 
 register_routes()
